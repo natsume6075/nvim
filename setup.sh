e07e95b8a568783faf69f112ca3e58130f29fd55
natsume6075 <natsume6075@gmail.com>
2022-02-28 11:25:44 +0900

    dein setup 自動化

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ mkdir -p ~/.local/share/nvim/backup
 mkdir -p ~/.local/share/nvim/undo
 
 # dein をインストール
-# curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-# sh ./installer.sh ~/.cache/dein
-# rm ./installer.sh
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm ./installer.sh
 
