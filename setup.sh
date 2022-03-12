5fca7139bf7a023f56ea1a96440879c0c86aecf4
natsume6075 <natsume6075@gmail.com>
2022-03-12 19:00:53 +0900

    install を分離

--- setup.sh
+++ setup.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+function GetConfirmation() {
+    echo "$1 (y/n)"
+    read input
+    if [ "$input" = 'y' ] ; then
+        return 1
+    elif [ "$input" = 'n' ] ; then
+        return 0
+    else
+        GetConfirmation "$1"
+    fi
+}
+
+
 mkdir -p ~/.config/
 ln -sfv $SCRIPT_DIR ~/.config/
 
 mkdir -p ~/.local/share/nvim/backup
 mkdir -p ~/.local/share/nvim/undo
 
-echo "Install dein? (y/[other])"
-read input
-if [ $input = 'y' ] ; then
+# If necessary, install dein.
+if ! GetConfirmation "Install dein?" ; then
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
     sh ./installer.sh ~/.cache/dein
     rm ./installer.sh
